5c93bd11f257981d6f9c511965dcd7d470f9c6dc
Jaiveer Singh <jaiveers@nvidia.com>
2022-06-30 23:26:54 -0700

    Isaac ROS 0.10.0 (DP)

--- isaac_ros_apriltag/launch/isaac_ros_argus_apriltag_pipeline.launch.py
+++ isaac_ros_apriltag/launch/isaac_ros_argus_apriltag_pipeline.launch.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2022, NVIDIA CORPORATION.  All rights reserved.
+# Copyright (c) 2022, NVIDIA CORPORATION.  All rights reserved.
 #
 # NVIDIA CORPORATION and its licensors retain all intellectual property
 # and proprietary rights in and to this software, related documentation
@@ -12,20 +12,45 @@ from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
+    argus_mono_node = ComposableNode(
+        name='argus_mono',
+        package='isaac_ros_argus_camera',
+        plugin='nvidia::isaac_ros::argus::ArgusMonoNode'
+    )
+
+    rectify_node = ComposableNode(
+        name='rectify_node',
+        package='isaac_ros_image_proc',
+        plugin='nvidia::isaac_ros::image_proc::RectifyNode',
+        parameters=[{
+            'output_width': 1920,
+            'output_height': 1200,
+        }],
+        remappings=[
+            ('image_raw', 'left/image_raw'),
+            ('camera_info', 'left/camerainfo')
+        ]
+    )
+
     apriltag_node = ComposableNode(
+        name='apriltag',
         package='isaac_ros_apriltag',
         plugin='nvidia::isaac_ros::apriltag::AprilTagNode',
-        name='apriltag',
-        parameters=[{'size': 0.16,
-                     'max_tags': 20}])
+        remappings=[
+            ('image', 'image_rect'),
+            ('camera_info', 'camera_info_rect')
+        ]
+    )
 
     apriltag_container = ComposableNodeContainer(
-        package='rclcpp_components',
         name='apriltag_container',
+        package='rclcpp_components',
         namespace='',
         executable='component_container_mt',
         composable_node_descriptions=[
-            apriltag_node,
+            argus_mono_node,
+            rectify_node,
+            apriltag_node
         ],
         output='screen'
     )
